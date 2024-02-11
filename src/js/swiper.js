c67350e5c38e7f8c287df06fec3d2faca693bb61
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-02-12 01:58:34 +0600

    first commit

---
+++ src/js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
